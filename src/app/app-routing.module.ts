346ea1a1d20f46db16d7d271a96aad398b5ae222
wovelscotch <wolvelscotch@yahoo.com>
2019-10-16 19:34:07 -0500

    added header and fp component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FrontPageBodyComponent } from './components/front-page-body/front-page-body.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FrontPageBodyComponent, pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/front-page-body/front-page-body.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-front-page-body',
+  templateUrl: './front-page-body.component.html',
+  styleUrls: ['./front-page-body.component.css']
+})
+export class FrontPageBodyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
