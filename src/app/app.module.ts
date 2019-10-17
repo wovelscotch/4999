21f02822d01db9bf569ce49d7eb24d57bb31ad94
wovelscotch <wolvelscotch@yahoo.com>
2019-10-17 13:24:10 -0500

    scaffold for different types moved plaintext input groups to their own component. Created components for each different auth type

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FrontPageBodyComponent } from './components/front-page-body/front-page-body.component';
+import { PlaintextComponent } from './components/plaintext/plaintext.component';
+import { SelectionComponent } from './components/selection/selection.component';
+import { ClickBasedComponent } from './components/click-based/click-based.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FrontPageBodyComponent
+    FrontPageBodyComponent,
+    PlaintextComponent,
+    SelectionComponent,
+    ClickBasedComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/click-based/click-based.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-click-based',
+  templateUrl: './click-based.component.html',
+  styleUrls: ['./click-based.component.css']
+})
+export class ClickBasedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
