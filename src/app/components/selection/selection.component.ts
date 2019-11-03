a926e0618ab70bdabd9dc0f050a917f23668899b
wovelscotch <wolvelscotch@yahoo.com>
2019-11-03 14:13:52 -0600

    output for selection module

--- src/app/components/selection/selection.component.ts
+++ src/app/components/selection/selection.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, NgModule } from '@angular/core';
+import { Component, OnInit, NgModule, EventEmitter, Output } from '@angular/core';
 
 export interface Tile {
   color: string;
@@ -21,20 +21,35 @@ export class SelectionComponent implements OnInit {
   images: string[] = [];
   NUM: number = 25;
   selectedIndex: number;
+  imageSelection: number[];
+  finalSelection: number[];
+
+  @Output() submission = new EventEmitter;
+
   ngOnInit() {
     for (let i = 0; i < this.NUM; i++) {
       let newRef = this.location + i + this.endtag;
       this.images.push(newRef);
     }
+    this.imageSelection = [];
+    this.finalSelection = [];
   }
   onImageClick(i: number) {
     this.selectedIndex = i;
+    this.imageSelection.push(i);
+    console.log(this.imageSelection);
 
   }
   onSubmitClick() {
-
+    if (this.imageSelection.length < 1) {
+      //error, needs at least one picture
+      console.log("error, needs to be at least one picture");
+    }
+    this.finalSelection = this.imageSelection;
+    this.imageSelection = [];
+    this.submission.emit(this.finalSelection);
   }
   onClearClick() {
-
+    this.imageSelection = [];
   }
 }
